5b4560c15d346f747db003267ba981a7097aeabb
ArshErgon <40994679+ArshErgon@users.noreply.github.com>
2022-04-04 11:30:25 +0530

    Update 217-Contains-Duplicate.py
    added two more solution with space and time complexcity

--- 217-Contains-Duplicate.py
+++ 217-Contains-Duplicate.py
@@ -7,3 +7,22 @@ class Solution:
                 return True
             hashset.add(n)
         return False
+
+  
+# 2 more Solutions
+
+
+# runtime: O(nlogn) space: O(1)
+def containsDuplicate(array):
+    array.sort()
+    for i in range(1, len(array)):
+        prevElement = array[i-1]
+        presentElement = array[i]
+        if prevElement == presentElement:
+            return True
+    return False
+
+# runtime: O(n)  space: O(1)
+def containsDuplicate(array):
+    return len(set(array)) == len(array)
+
